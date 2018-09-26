2ecdec4132a2c5372afa12a3849c01dd2e7e483c
prat96 <pratheekb96@gmail.com>
2018-09-26 19:05:40 +0530

    Gain matrix output added.

--- python/gain.py
+++ python/gain.py
@@ -4,30 +4,6 @@ import numpy as np
 from read_pgm_file import get_data
 from read_pgm_file import read_pgm
 
-"""
-directory = './datasets/sample/'
-i = 0
-avgimg = 0
-
-for d in os.walk(directory):
-    #print(d)
-    while i < 60:
-        filename = d[2][i]
-        #print filename 
-        image = np.array(get_data('./datasets/sample/' + filename))
-        #print image
-
-        avgimg = image + avgimg
-        avgimg = avgimg/2
-        i = i + 1
-
-np.absolute(avgimg)
-print np.around(avgimg, decimals=-1)
-print avgimg.shape
-avgimgval = np.average(avgimg)
-print np.around(avgimgval, decimals=-1)
-"""
-
 
 def lowtemp_avg():
     directory = '../datasets/gain/low_temp/'
@@ -39,18 +15,16 @@ def lowtemp_avg():
         while i < 60:
             filename = d[2][i]
             # print filename
-            image = np.array(get_data('../datasets/gain/low_temp/' + filename))
-            # print image
+            image = np.array(get_data('../datasets/gain/low_temp/' + filename), dtype = np.uint16)
+            print(image)
             avgimg_low = image + avgimg_low
             avgimg_low = avgimg_low / 2
             i = i + 1
 
-    np.absolute(avgimg_low)
-    print(np.around(avgimg_low, decimals=0))
     print(avgimg_low.shape)
     avgimg_lowval = np.average(avgimg_low)
-    print(np.around(avgimg_lowval, decimals=0))
-
+    print(avgimg_lowval)
+    global avgimg_low
 
 def hightemp_avg():
     directory = '../datasets/gain/high_temp/'
@@ -62,19 +36,33 @@ def hightemp_avg():
         while i < 60:
             filename = d[2][i]
             # print filename
-            image = np.array(get_data('../datasets/gain/high_temp/' + filename))
+            image = np.array(get_data('../datasets/gain/high_temp/' + filename), dtype = np.uint16)
             # print image
-            avgimg_high = image + avgimg_high   
+            avgimg_high = image + avgimg_high
             avgimg_high = avgimg_high / 2
             i = i + 1
 
-    np.absolute(avgimg_high)
-    print(np.around(avgimg_high, decimals=0))
     print(avgimg_high.shape)
     avgimg_highval = np.average(avgimg_high)
-    print(np.around(avgimg_highval, decimals=0))
+    print(avgimg_highval)
+    global avgimg_high
 
+def compute_gain():
+    mlow = np.median(avgimg_low)
+    print('median of low = ', mlow)
+    mhigh = np.median(avgimg_high)
+    print('median of high =', mhigh)
+
+    avgdiff = (avgimg_high-avgimg_low)
+    print('avgdiff')
+    print(avgdiff)
+
+    gain_mat = ((mhigh-mlow)/(avgimg_high-avgimg_low))
+    np.abs(gain_mat)
+    print('gain_mat -->')
+    print(gain_mat)
 
 if __name__ == '__main__':
     lowtemp_avg()
     hightemp_avg()
+    compute_gain()
