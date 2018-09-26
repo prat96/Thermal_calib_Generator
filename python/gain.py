7f363e04736e6cc256c65cc900125661be356b49
prat96 <pratheekb96@gmail.com>
2018-09-26 14:33:51 +0530

    Ported to pycharm + python3.5

--- python/gain.py
+++ python/gain.py
@@ -3,6 +3,7 @@ import numpy as np
 
 from read_pgm_file import get_data
 from read_pgm_file import read_pgm
+
 """
 directory = './datasets/sample/'
 i = 0
@@ -27,55 +28,53 @@ avgimgval = np.average(avgimg)
 print np.around(avgimgval, decimals=-1)
 """
 
-def lowtemp_avg():
 
-    directory = './datasets/gain/low_temp/'
+def lowtemp_avg():
+    directory = '../datasets/gain/low_temp/'
     i = 0
     avgimg_low = 0
 
     for d in os.walk(directory):
-    #print(d)
+        # print(d)
         while i < 60:
             filename = d[2][i]
-        #print filename 
-            image = np.array(get_data('./datasets/gain/low_temp/' + filename))
-            #print image
+            # print filename
+            image = np.array(get_data('../datasets/gain/low_temp/' + filename))
+            # print image
             avgimg_low = image + avgimg_low
-            avgimg_low = avgimg_low/2
+            avgimg_low = avgimg_low / 2
             i = i + 1
 
     np.absolute(avgimg_low)
-    print np.around(avgimg_low, decimals=0)
-    print avgimg_low.shape
+    print(np.around(avgimg_low, decimals=0))
+    print(avgimg_low.shape)
     avgimg_lowval = np.average(avgimg_low)
-    print np.around(avgimg_lowval, decimals=0)
+    print(np.around(avgimg_lowval, decimals=0))
+
 
 def hightemp_avg():
-
-    directory = './datasets/gain/high_temp/'
+    directory = '../datasets/gain/high_temp/'
     i = 0
     avgimg_high = 0
 
     for d in os.walk(directory):
-    #print(d)
+        # print(d)
         while i < 60:
             filename = d[2][i]
-        #print filename 
-            image = np.array(get_data('./datasets/gain/high_temp/' + filename))
-            #print image
-            avgimg_high = image + avgimg_high
-            avgimg_high = avgimg_high/2
+            # print filename
+            image = np.array(get_data('../datasets/gain/high_temp/' + filename))
+            # print image
+            avgimg_high = image + avgimg_high   
+            avgimg_high = avgimg_high / 2
             i = i + 1
 
     np.absolute(avgimg_high)
-    print np.around(avgimg_high, decimals=0)
-    print avgimg_high.shape
+    print(np.around(avgimg_high, decimals=0))
+    print(avgimg_high.shape)
     avgimg_highval = np.average(avgimg_high)
-    print np.around(avgimg_highval, decimals=0)
+    print(np.around(avgimg_highval, decimals=0))
 
 
 if __name__ == '__main__':
-    
     lowtemp_avg()
     hightemp_avg()
-    print 'yay!'
