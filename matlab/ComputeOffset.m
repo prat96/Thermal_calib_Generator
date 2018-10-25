331c2dadda2e695d5d92f4352a94d064f175aadc
prat96 <pratheekb96@gmail.com>
2018-10-25 18:26:37 +0530

    .gitignore is now working

--- matlab/ComputeOffset.m
+++
@@ -1,9 +0,0 @@
-function [ OffsetMat ] = ComputeOffset ( GainMat, AvgImgMat, OffsetPath, T_amb )
-  GI = GainMat.*AvgImgMat; % GI => Gain Corrected Image
-  medianGI = median(GI(:));
-  OffsetMat =  round( 100000 *  ( GI - medianGI )) ./ 100000; % This is Offset Coefficient for the image to which nuc has to apply
-  
-  OffsetName = sprintf( 'Offset_Mats/Offset_%d.mat', T_amb );
-  OffsetFile = strcat( OffsetPath, OffsetName );
-  dlmwrite( OffsetFile, OffsetMat, ' ' );
-end
--- matlab/PruneMatrix.m
+++
@@ -1,3 +0,0 @@
-function [ Pruned_Mat ] = PruneMatrix( Raw_Mat, height, width, prefix, suffix )
-  Pruned_Mat = Raw_Mat( : , prefix + 1 : end - suffix );
-end
